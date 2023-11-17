eec00cff63cd2182ca93136b19b081adfc47a6ea
Anchor <donquixote1110@outlook.com>
2023-11-17 16:39:23 +0800

    11-17

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/dcm-viewer/" : "/",
   plugins: [
     vue(),
     createSvgIconsPlugin({

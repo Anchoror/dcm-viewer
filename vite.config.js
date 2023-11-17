5c4290c34b2a44c986de1573fb6339f76944822d
Anchor <donquixote1110@outlook.com>
2023-11-17 20:36:49 +0800

    11-17

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "./" : "/",
+  base:
+    process.env.NODE_ENV === "production" ? "https://anchoror.github.io/" : "/",
   plugins: [
     vue(),
     createSvgIconsPlugin({
